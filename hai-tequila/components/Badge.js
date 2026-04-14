58541ff2b52112b25264a44d549d3b92340cbff9
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2026-04-14 23:29:05 +0700

    Fix syntax error in BlogCard.js and Badge.js

--- hai-tequila/components/Badge.js
+++ hai-tequila/components/Badge.js
@@ -1,6 +1,6 @@
 export default function Badge({ label, color = "indigo" }) {
   const styles = color === "indigo" ? "bg-indigo-100 text-indigo-700" : "bg-gray-100 text-gray-600";
   return (
-    <span className={'$(styles) text-xs font-semibold px-2 py-1 rounded-full'}>(label)</span>
+    <span className={`${styles} text-xs font-semibold px-2 py-1 rounded-full`}>{label}</span>
   );
 }
