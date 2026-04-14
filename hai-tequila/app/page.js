261016c6fff7cc0537d8a6035a7ae00ee4f07502
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2026-04-14 23:26:12 +0700

    modify page.js

--- hai-tequila/app/page.js
+++ hai-tequila/app/page.js
@@ -7,9 +7,11 @@ async function getPosts() {
 
 export default async function HomePage() {
   const posts = await getPosts();
-
-  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+  return (<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
     {posts.slice(0, 12).map(post => (
       <BlogCard key={post.id} post={post} />))}
   </div>
+    //console.log(posts.length, posts[0])
+  )
+
 }
