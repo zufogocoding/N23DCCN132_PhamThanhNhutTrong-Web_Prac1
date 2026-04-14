c959df33ea3dd48a588adfd22742936916087c1e
zufogocoding <phamthanhnhuttrong123456@gmail.com>
2026-04-14 19:45:56 +0700

    create and working with file Header.js

---
+++ hai-tequila/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center p-6 bg-white shadow-md">
+      <h1 className="text-2xl font-bold text-indigo-600">MyBlog</h1>
+      <nav className="space-x-6 hidden md:flex">
+        <a href="#" className="text-indigo-500">Home</a>
+        <a href="#" className="text-indigo-500">Articles</a>
+        <a href="#" className="text-indigo-500">About</a>
+        <a href="#" className="text-indigo-500">Home</a>
+      </nav>
+      <button className="bg-indigo-600 text-white px-4 py-2 rounded-lg">
+        Subscribe
+      </button>
+    </header>
+  );
+}
